94a7c98987b97b7932eceea5b78733190b75e3d8
Kim JH Desktop <kim_jh6859@naver.com>
2023-07-18 23:33:14 +0900

    edit

---
+++ front/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+let data: number = 1;
+const App = () => {
+  return (
+    <div className="App">
+      {data}
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/model/data.ts
@@ -0,0 +1,3 @@
+export type Number = {
+    data : number
+}
